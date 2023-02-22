07722c21e604edad11d7cbd8f0fce67cb686ba07
donshester <vlad.stepanov.2003@bk.ru>
2023-02-22 20:42:37 +0300

    add: operation function added

--- Lab1/tasks.py
+++ Lab1/tasks.py
@@ -1,2 +1,18 @@
 print('Hello world!')
 
+def operation_function(num_1, num_2, operation):
+    if operation == 'add':
+        return num_1+num_2
+
+    elif operation == 'sub':
+        return num_1-num_2
+
+    elif operation == 'mul':
+        return num_1*num_2
+
+    elif operation == 'div':
+        return num_1/num_2
+    
+    else: 
+        return 'Incorrect operation'    
+
