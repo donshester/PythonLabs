d2a5e9c48d15d6cc590881e8276e3c39763827a3
donshester <vlad.stepanov.2003@bk.ru>
2023-03-08 17:45:15 +0300

    add: count avr letters and declarative sentences

--- Lab2/task1/main.py
+++ Lab2/task1/main.py
@@ -1,3 +1,9 @@
 import textStatistics
 
 text = input('Enter text:')
+
+text= textStatistics.transform_text(text)
+sentences = textStatistics.find_sentences(text)
+declarative_sentences = textStatistics.count_declarartive_sentences(sentences)
+print(declarative_sentences)
+print(textStatistics.count_words_size(text))
--- Lab2/task1/textStatistics.py
+++ Lab2/task1/textStatistics.py
@@ -5,7 +5,21 @@ def transform_text(text):
         raise Exception('Only Latin letters, numbers and separators can be processed!')
     return text
 
-def count_sentences(text):
+def find_sentences(text):
     sentences = re.split('(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\!)\s', text)
     
     return sentences
+
+def count_declarartive_sentences(sentences):
+    return len([dec_sentence for dec_sentence in sentences if dec_sentence.endswith('.') or dec_sentence.endswith('...')])
+
+
+def count_words_size(text):
+    words = re.findall(r'\b(?![.,\d])\w*[a-zA-Z]\w*\b', text)
+
+    sum_letters=0
+    for word in words:
+        sum_letters+=len(word)
+    
+    return sum_letters/len(words)
+
