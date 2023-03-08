57fb925fff3d46732f088e807de2afb9514ce5ea
donshester <vlad.stepanov.2003@bk.ru>
2023-03-08 23:08:22 +0300

    add: other functions

--- Lab2/task1/main.py
+++ Lab2/task1/main.py
@@ -1,9 +1,35 @@
 import textStatistics
 
 text = input('Enter text:')
+N = input('Enter N:')
+K = input('Enter K:')
+
+try: 
+    N= int(N)
+    K = int(K)
+except ValueError:
+    flag = True 
 
 text= textStatistics.transform_text(text)
+
 sentences = textStatistics.find_sentences(text)
-declarative_sentences = textStatistics.count_declarartive_sentences(sentences)
-print(declarative_sentences)
-print(textStatistics.count_words_size(text))
+declarative_sentences_length = textStatistics.count_declarartive_sentences(sentences)
+
+non_declarative_sentences_length = len(sentences) - declarative_sentences_length
+
+words, avr_length = textStatistics.count_words_size(text)
+avr_sentence_length = textStatistics.count_sentence_length(words, sentences)
+
+if flag:
+    top_ngrams, N, K = textStatistics.top_k_ngramms(words)   
+else:
+    top_ngrams, N, K = textStatistics.top_k_ngramms(words, N, K)   
+    
+
+print(f'Amount of sentences: {len(sentences)}')
+print(f'Amount of non-declarative sentences: {non_declarative_sentences_length}')
+print(f'Average length of the sentence: {avr_sentence_length}')
+print(f'Average length of the word: {avr_length}')
+print(f"Top {K} repeated {N}-grams:")
+for ngram, count in top_ngrams:
+    print(f"{ngram}: {count}")
--- Lab2/task1/textStatistics.py
+++ Lab2/task1/textStatistics.py
@@ -1,14 +1,19 @@
 import re
+from collections import Counter
 
 def transform_text(text):
-    if(len(re.findall(r'[^\x00-\x7F]+', text))):  # Only latin letters, numbers and separators
-        raise Exception('Only Latin letters, numbers and separators can be processed!')
+    # Only latin letters, numbers and separators
+    if len(re.findall(r'[^\x00-\x7F]+', text)):
+        raise Exception(
+            'Only Latin letters, numbers and separators can be processed!')
     return text
+
 
 def find_sentences(text):
     sentences = re.split('(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\!)\s', text)
-    
+
     return sentences
+
 
 def count_declarartive_sentences(sentences):
     return len([dec_sentence for dec_sentence in sentences if dec_sentence.endswith('.') or dec_sentence.endswith('...')])
@@ -17,9 +22,26 @@ def count_declarartive_sentences(sentences):
 def count_words_size(text):
     words = re.findall(r'\b(?![.,\d])\w*[a-zA-Z]\w*\b', text)
 
-    sum_letters=0
+    sum_letters = 0
     for word in words:
-        sum_letters+=len(word)
-    
-    return sum_letters/len(words)
+        sum_letters += len(word)
 
+    return words, sum_letters/len(words)
+
+
+def count_sentence_length(words, sentences):
+    sum_of_letters = 0
+    for word in words:
+        sum_of_letters += len(word)
+
+    return sum_of_letters/len(sentences)
+
+def top_k_ngramms(words, K=10, N=4):
+    ngrams = []
+    for i in range(len(words) - N + 1):
+        ngram = " ".join(words[i:i+N])
+        ngrams.append(ngram)    
+
+    top_ngrams = Counter(ngrams).most_common(K)
+
+    return top_ngrams, K, N
