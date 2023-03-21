d75b7845963282c0008c6ddcaa6af9a152e9d863
donshester <vlad.stepanov.2003@bk.ru>
2023-03-22 02:51:03 +0300

    feat: add basic logic for container

--- Lab2/task2/entities/container.py
+++ Lab2/task2/entities/container.py
@@ -1,2 +1,46 @@
+import re
+
 class Container:
-    pass  
+
+    def __init__(self) -> None:
+        self.__data = set()
+
+    @property
+    def data(self):
+        return self.__data
+    
+    @data.setter
+    def data(self, new_set: set):
+        self.__data = new_set
+
+    def add(self, *elems):
+        self.data.add(*elems)
+    
+    def remove(self, elem):
+        try:
+            self.data.remove(elem)
+        except KeyError:
+            print("No such key in container!")
+
+    def find(self, elem):
+        return elem if elem in self.data else "Element not found!"
+    
+        
+    def list(self) -> list:
+        return list(self.data)
+    
+    def grep(self, regex):
+        pattern = re.compile(regex)
+        founded = []
+        
+        for element in self.data:
+            if pattern.search(element): 
+                founded.append(element)
+            
+        return founded
+
+    def save(self):
+        pass
+        
+    def switch(self):
+        pass
