ebae4106d989eda89b67c0a7ff763e2e12883fae
donshester <vlad.stepanov.2003@bk.ru>
2023-03-22 21:29:12 +0300

    feat: user realization previous functions of conatainer and fixed bugs

--- Lab2/task2/container.py
+++ Lab2/task2/container.py
@@ -1,5 +1,6 @@
 import re
 
+
 class Container:
 
     def __init__(self) -> None:
@@ -8,14 +9,14 @@ class Container:
     @property
     def data(self):
         return self.__data
-    
+
     @data.setter
     def data(self, new_set: set):
         self.__data = new_set
 
     def add(self, *elems):
         self.data.add(*elems)
-    
+
     def remove(self, elem):
         try:
             self.data.remove(elem)
@@ -24,23 +25,22 @@ class Container:
 
     def find(self, elem):
         return elem if elem in self.data else "Element not found!"
-    
-        
+
     def list(self) -> list:
         return list(self.data)
-    
+
     def grep(self, regex):
         pattern = re.compile(regex)
         founded = []
-        
+
         for element in self.data:
-            if pattern.search(element): 
+            if pattern.search(element):
                 founded.append(element)
-            
+
         return founded
 
     def save(self):
         pass
-        
+
     def switch(self):
         pass
---
+++ Lab2/task2/user.py
@@ -0,0 +1,44 @@
+from container import Container
+
+
+class User:
+
+    def __init__(self, name) -> None:
+        self._user_name = name
+        self._container = Container()
+
+    @property
+    def user_name(self):
+        return self.user_name
+
+    @property
+    def container(self):
+        return self._container
+
+    @user_name.setter
+    def user_name(self, name):
+        self.user_name = name
+
+    def add_elements(self, *elems):
+        self.container.add(elems)
+
+    def remove_element(self, elem):
+        self.container.remove(elem)
+
+    def find_element(self, elem):
+        self.container.find(elem)
+
+    def grep_elements(self, regex):
+        self.container.grep(regex)
+
+    def list_data(self):
+        print(self.container.list())
+
+    def save(self):
+        pass
+
+    def load(self):
+        pass
+
+    def switch(self):
+        pass
